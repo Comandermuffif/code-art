e265003529bba2cdd407be79794ee74b588b1aa3
Vincent Coffey <vincent3coffey@gmail.com>
2022-11-20 09:41:05 -0500

    fixup! fixup! Get GUI working

--- gui.py
+++ gui.py
@@ -15,49 +15,34 @@ import tkinter
 
 from PIL import Image, ImageTk
 
-def get_image(surface: cairo.ImageSurface, context:cairo.Context) -> ImageTk.PhotoImage:    
-    context.set_source_rgb(random.random(), random.random(), random.random())
-    context.arc(512, 512, 50, 0, 360)
-    context.fill()
-    data = Image.frombuffer("RGBA", (surface.get_width(), surface.get_height()), surface.get_data().tobytes(), "raw", "BGRA", 0, 1)
-    return ImageTk.PhotoImage(data)
+class DrawUI(tkinter.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.width, self.height = 1024, 1024
+
+        self.geometry("{}x{}".format(self.width, self.height))
+
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.width, self.height)
+        self.context = cairo.Context(self.surface)
+        self.label:tkinter.Label = None
+        self.button = tkinter.Button(self, text="Generate", command=self.draw)
+        self.button.pack()
+
+    def draw(self):
+        if self.label:
+            self.label.destroy()
+
+        self.context.set_source_rgb(random.random(), random.random(), random.random())
+        self.context.arc(512, 512, 50, 0, 360)
+        self.context.fill()
+
+        self._image_ref = ImageTk.PhotoImage(Image.frombuffer("RGBA", (self.width, self.height), self.surface.get_data().tobytes(), "raw", "BGRA", 0, 1))
+        self.label = tkinter.Label(self, image=self._image_ref)
+        self.label.pack(expand=True, fill="both")
 
 def main():
     arguments = docopt.docopt(__doc__, version='v0.0.0')
-    window = tkinter.Tk()
-
-    width, height = 1024, 1024
-    window.geometry(f"{width}x{height}")
-    window.resizable(False, False)
-
-    min_hue = tkinter.Scale(window, from_=0, to=100, orient=tkinter.HORIZONTAL, length=width/2)
-    max_hue = tkinter.Scale(window, from_=0, to=100, orient=tkinter.HORIZONTAL, length=width/2)
-    image_label = tkinter.Label(window, text="1")
-
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
-    context = cairo.Context(surface)
-
-    settings = {
-      "a": get_image(surface, context),
-      "b": get_image(surface, context),
-    }
-    settings["current_image"] = settings["a"]
-
-    def gen_callback():
-        print("Button start")
-        settings["current_image"] = settings["b"] if settings["current_image"] == settings["a"] else settings["a"]
-        image_label.config(image=settings["current_image"])
-        print("Button end")
-
-    gen_button = tkinter.Button(window, text="Generate", command=gen_callback)
-
-    image_label.config(image=settings["current_image"])
-
-    min_hue.pack()
-    max_hue.pack()
-    gen_button.pack()
-    image_label.pack(expand=True, fill="both")
-
+    window = DrawUI()
     window.mainloop()
 
 if __name__ == '__main__':
