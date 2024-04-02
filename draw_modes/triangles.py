456c1de6fa247345b169731025a9db1661e4fe72
Vincent Coffey <vincent3coffey@gmail.com>
2024-04-01 21:25:42 -0400

    Rework trianges so it doesn't have gaps

--- draw_modes/triangles.py
+++ draw_modes/triangles.py
@@ -8,62 +8,33 @@ class TrianglesDrawMode(DrawMode):
         self.count = count
 
     def draw(self, context:cairo.Context, color_mode:ColorMode, width:int, height:int) -> None:
-        step = width/self.count
+        xStep = width / self.count
+        yStep = width / self.count
 
-        points_a = [
+        points = [
             (0, 0),
             (1, 0),
-            (0.5, 1),
+            (0, 1),
+            (1, 1)
         ]
-        center_a = (sum([x[0] for x in points_a])/len(points_a), sum([x[1] for x in points_a])/len(points_a))
 
-        points_b = [
-            (1, 0),
-            (1.5, 1),
-            (0.5, 1),
+        offsets = [
+            0.25,
+            0.75,
         ]
-        center_b = (sum([x[0] for x in points_b])/len(points_b), sum([x[1] for x in points_b])/len(points_b))
 
-        for count_x in range(self.count):
-            for count_y in range(self.count):
-
-                if count_y % 2 == 0:
-                    color = color_mode.getColor((count_x + center_a[0])/self.count, (count_y + center_a[1])/self.count)
+        for xCount in range(self.count):
+            for yCount in range(self.count):
+                for offsetIndex in range(len(offsets)):
+                    color = color_mode.getColor(
+                        (xCount + offsets[offsetIndex])/self.count,
+                        (yCount + offsets[offsetIndex])/self.count
+                    )
                     context.set_source_rgba(*color.toTuple())
 
-                    context.move_to((count_x + points_a[0][0]) * step, (count_y + points_a[0][1]) * step)
-                    context.line_to((count_x + points_a[1][0]) * step, (count_y + points_a[1][1]) * step)
-                    context.line_to((count_x + points_a[2][0]) * step, (count_y + points_a[2][1]) * step)
-                    context.close_path()
-                    context.fill_preserve()
-                    context.stroke()
-
-                    color = color_mode.getColor((count_x + center_b[0])/self.count, (count_y + center_b[1])/self.count)
-                    context.set_source_rgba(*color.toTuple())
-
-                    context.move_to((count_x + points_b[0][0]) * step, (count_y + points_b[0][1]) * step)
-                    context.line_to((count_x + points_b[1][0]) * step, (count_y + points_b[1][1]) * step)
-                    context.line_to((count_x + points_b[2][0]) * step, (count_y + points_b[2][1]) * step)
-                    context.close_path()
-                    context.fill_preserve()
-                    context.stroke()
-                else:
-                    color = color_mode.getColor((count_x + center_a[0])/self.count, (count_y + center_a[1])/self.count)
-                    context.set_source_rgba(*color.toTuple())
-
-                    context.move_to((count_x + points_a[0][0]) * step, (count_y + 1 - points_a[0][1]) * step)
-                    context.line_to((count_x + points_a[1][0]) * step, (count_y + 1 - points_a[1][1]) * step)
-                    context.line_to((count_x + points_a[2][0]) * step, (count_y + 1 - points_a[2][1]) * step)
-                    context.close_path()
-                    context.fill_preserve()
-                    context.stroke()
-
-                    color = color_mode.getColor((count_x + center_b[0])/self.count, (count_y + center_b[1])/self.count)
-                    context.set_source_rgba(*color.toTuple())
-
-                    context.move_to((count_x + points_b[0][0]) * step, (count_y + 1 - points_b[0][1]) * step)
-                    context.line_to((count_x + points_b[1][0]) * step, (count_y + 1 - points_b[1][1]) * step)
-                    context.line_to((count_x + points_b[2][0]) * step, (count_y + 1 - points_b[2][1]) * step)
+                    context.move_to((xCount + points[0 + offsetIndex][0]) * xStep, (yCount + points[0 + offsetIndex][1]) * yStep)
+                    context.line_to((xCount + points[1 + offsetIndex][0]) * xStep, (yCount + points[1 + offsetIndex][1]) * yStep)
+                    context.line_to((xCount + points[2 + offsetIndex][0]) * xStep, (yCount + points[2 + offsetIndex][1]) * yStep)
                     context.close_path()
                     context.fill_preserve()
                     context.stroke()
